8099f4c1046fa27599f2a3dff48565a0908192c3
seforarodriguez <seforarodriguez13@gmail.com>
2016-03-03 14:27:59 -0600

    Messages are staying live, but not saved in db yet

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,49 @@
-io.connect()
+(() => {
+  'use strict'
+  const ws = io.connect()
+
+  ws.on('connect', socket => {
+    console.log('socket conencted');
+  })
+
+  const form = document.querySelector('form')
+  const name = document.querySelector('input[name="name"]')
+  const text = document.querySelector('input[name="text"]')
+  const ul = document.querySelector('ul')
+
+  form.addEventListener('submit', () => {
+    const [n, t] = [name.value, text.value]
+    
+    ws.emit('sendChat', {
+      name: n,
+      text: t
+    })
+
+    displayChat(n, t)  
+
+    event.preventDefault()
+  })
+
+  function displayChat () {
+    const li = generateLI(name.value, text.value)
+
+    text.value = ''
+    ul.appendChild(li)
+  }
+
+  function generateLI (name, text) {
+    const li = document.createElement('li')
+    const textNode = document.createTextNode(`${name}: ${text}`)
+
+    li.appendChild(textNode)
+    return li
+  }
+
+  function getJSON(url, db) {
+    const request = new XMLHttpRequest()
+    request.open('GET', url)
+
+    request
+  }
+
+})();
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const express = require('express')
-  ,    app = express()
+  ,   app = express()
+  ,   pg = require('pg').native
   ,   server = require('http').createServer(app)
   ,   ws = require('socket.io')(server)
   ,   PORT = process.env.PORT || 3000
+  ,   POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://localhost:5432/nodechat'
+
+let db
 
 app.set('view engine', 'jade')
 
@@ -14,10 +18,29 @@ app.get('/', (req, res) => {
     res.render('index')
   })
 
+app.get('/chats', (req, res) => {
+  db.query('SELECT * FROM chats', (err, result) => {
+    if (err) throw err
+
+    res.send(result.rows)
+  })
+})
+
+pg.connect(POSTGRES_URL, (err, client) => {
+  if (err) throw err
+
+  db = client
+
 server.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
   })
+})
 
 ws.on('connection', socket => {
   console.log('socket Connected')
+
+  socket.on('sendChat', (msg) => {
+    console.log(msg)
+     socket.broadcast.emit('receiveChat', msg)
+  })
 })
